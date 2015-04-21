29352aa6c728f2310ab03e0407fddeeda1ab0be3
muratkrty <muratkrty@gmail.com>
2015-04-21 12:43:24 +0300

    Added convert2eps

---
+++ mics/convert2eps.m
@@ -0,0 +1,14 @@
+function [ ] = convert2Eps(image)
+% Author: Murat Kirtay, Robotics Laboratory
+% Date: 21/04/2015
+% Description: conver *jpg, *png files to eps format
+% Input/s: image
+% Output/s: image_eps, saved image in .eps format
+% Bugs: No known.
+
+[img, map] = imread(image);
+
+image_eps = strcat(image, '_.eps');
+saveas(h,image_eps, 'psc2')
+end
+
